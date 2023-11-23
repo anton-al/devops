f3f1a6ff2cb8fe4a4e9ba1998bb02c6717289794
Anton Alputov <anton.alputov@sharp-dev.net>
2023-11-23 20:05:54 +0530

    add app action

--- aws-cdk/cdk-cicd/lib/cdk-cicd-stack.ts
+++ aws-cdk/cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('anton-al/devops', 'cicd-practice'),
+        commands: [
+          'cd aws-cdk/cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'aws-cdk/cdk-cicd/cdk.out'
+      })
+    })
   }
 }
