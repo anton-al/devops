4aa34bd70d8ee2c67bab9dfbfc091a0312f6f4ba
Anton Alputov <anton.alputov@sharp-dev.net>
2023-11-23 22:41:59 +0530

    break unit test

--- aws-cdk/cdk-cicd/test/cdk-cicd.test.ts
+++ aws-cdk/cdk-cicd/test/cdk-cicd.test.ts
@@ -3,6 +3,6 @@ import {handler} from "../services/hello";
 describe('Hello describe test suite', () => {
   test('handler should return 200', async () => {
     const response = await handler({}, {});
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(400);
   })
 })
