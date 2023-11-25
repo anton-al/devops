cfa53ca5360080ced98bc46522ad0c4e8c3f22a2
Anton Alputov <anton.alputov@sharp-dev.net>
2023-11-25 16:21:46 +0530

    sdk s3 client

--- aws-cdk/cdk-space-finder/src/infra/stacks/LambdaStack.ts
+++ aws-cdk/cdk-space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,28 +1,40 @@
 import {Stack, StackProps} from "aws-cdk-lib";
-import {Code, Function as LambdaFunction, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 import {Construct} from "constructs";
 import {join} from "path";
 import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
 import {ITable} from "aws-cdk-lib/aws-dynamodb";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 
 interface LambdaStackProps extends StackProps {
   spacesTable: ITable
 }
 
-export class LambdaStack extends Stack{
+export class LambdaStack extends Stack {
 
   public readonly helloLambdaIntegration: LambdaIntegration;
+
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "HelloLambda", {
-      code: Code.fromAsset(join( __dirname, '..', '..', 'services') ),
-      handler: "hello.main",
+    const helloLambda = new NodejsFunction(this, "HelloLambda", {
+      entry: (join(__dirname, '..', '..', 'services', 'hello.ts')),
+      handler: "handler",
       runtime: Runtime.NODEJS_18_X,
       environment: {
         TABLE_NAME: props.spacesTable.tableName
       }
     });
+
+    helloLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        's3:ListAllMyBuckets',
+        's3:ListBucket'
+      ],
+      resources: ["*"] // bad practice
+    }));
 
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
   }
